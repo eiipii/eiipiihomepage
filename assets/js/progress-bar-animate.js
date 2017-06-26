26cebf17f57c4369edd495aa2fa91f22a80f0495
michalrobertgora <michal.robert.gora@gmail.com>
2017-06-26 13:07:54 +0200

    commented out stub of script to animate progress bars on slide change, with comments what needs to be done in future - LOW PRIORITY

--- assets/js/progress-bar-animate.js
+++ assets/js/progress-bar-animate.js
@@ -1,3 +1,12 @@
+/*
 $(document).ready(function(){
     $(".progress-bar").removeClass("zero-width");
 });
+*/
+
+// TODO animation. steps: 1 add zero-width class to every progress-bar; 2 make script below working, acting on slide load event
+$('#carousel-personas').on('slide.bs.carousel', function () {
+    console.log("it logs");
+    $(".progress-bar").removeClass("zero-width");
+});
+
