9da942bf71716de7d437b2531d2194e4555622da
michalrobertgora <michal.robert.gora@gmail.com>
2017-06-26 13:19:27 +0200

    minor change in scroll js - moved anchor 30px to make margin safer / nicer to look at

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -59,7 +59,7 @@ $(function () {
     $(document).on('click', 'a.page-scroll', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top-30
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
