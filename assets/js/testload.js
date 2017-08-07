391a56b7b7d062df51822ea2e7f257518dea1e3d
Paweł Cesar Sanjuan Szklarz <p@eiipii.com>
2017-08-07 12:12:10 +0200

    tester path

--- assets/js/testload.js
+++ assets/js/testload.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var graph = $.get('assets/techtree.gv', function(data) {
+    var graph = $.get('assets/testerLearnTree.gv', function(data) {
         $("#div_graph").append(Viz(data, {format:"svg", engine:"dot"}));
     }, 'text');
 });
